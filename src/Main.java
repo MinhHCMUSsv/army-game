0bb11f80cb74bdad74478249734e90f69c2108e7
MinhHCMUSsv <minhtn23122005@gmail.com>
2026-03-14 15:39:35 +0700

    Add: Composite pattern for army and mermaid diagram

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import soldier.Cavalry;
 import soldier.Infantry;
 import equipment.Sword;
 import equipment.Shield;
+import composite.Army;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,5 +26,19 @@ public class Main {
         strength = infantry.hit();
         System.out.println(strength);
         cavalry.wardOff(strength);
+
+        // Test Army
+        Army group1 = new Army("Vikings");
+        group1.add(infantry);
+        group1.add(cavalry);
+        group1.hit();
+        group1.wardOff(100);
+
+        Army group2 = new Army("Samurai");
+        group2.add(new Infantry());
+        group2.add(new Cavalry());
+        group2.hit();
+        group2.wardOff(150);
+
     }
 }
